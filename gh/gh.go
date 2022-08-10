70ab7a99c1141def9e98faf1d2a1c4a7ef09a219
wanghui <wh_linux@126.com>
2022-08-10 10:51:01 +0800

    docs: add how to provide a gin.RouterGroup and then use it

--- gh/gh.go
+++ gh/gh.go
@@ -17,7 +17,33 @@ import (
 
 var routerGroupProvided bool
 
-// ProvideRouterGroup define a router group
+// ProvideRouterGroup define a router group and inject it into the runtime,
+// then you can use it anywhere during the project lifecyle.
+//
+// First, you need to use a function to create a `gin.RouterGroup`:
+//
+//     func init() {
+//     	gh.ProvideRouterGroup("api", func(app *gh.App) *gin.RouterGroup {
+//     		return app.Engine.Group("api")
+//     	})
+//     }
+//
+// Then, you can use it anywhere with DI:
+//
+//     type routerParams struct {
+//     	fx.In
+//
+//     	Default *gin.RouterGroup `name:"api"`
+//     }
+//
+//     func init() {
+//     	di.Invoke(func(r routerParams) {
+//     		r.Default.GET("/", func(c *gin.Context) {
+//     			c.Writer.WriteString("Hello, world!")
+//     		})
+//     	})
+//     }
+//
 func ProvideRouterGroup(groupName string, fn interface{}) {
 	t := reflect.TypeOf(fn)
 
